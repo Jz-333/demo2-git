fad74b9ecece29bc4f1f34278d0c1ba4b98edce5
jz333 <2218341249@qq.com>
2020-03-23 22:05:11 +0800

    项目经理第二次修改项目demo2

---
+++ src/components/Watch.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="Watch">
+        用户名：<input type="text" @input="handle($event)">{{msg}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Watch",
+        data:function(){
+            return {
+                username:'',
+                msg:''
+            }
+        },
+        methods:{
+            handle:function(event){
+                this.username = event.target.value;
+                this.check();
+            },
+            check:function(){
+                setTimeout(function(){
+                    if(this.username === "admin"){
+                        this.msg = "用户名重复，请重新输入！"
+                    }else{
+                        this.msg = "该用户名可用！"
+                    }
+                },2000);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    input{
+        width:200px;
+        height:30px;
+        padding:0 10px;
+    }
+</style>
